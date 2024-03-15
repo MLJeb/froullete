23f65481f5d86d538e03d34ccb2a41d8308cf80b
Jesus Beltran <jebeltranuseche@gmail.com>
2024-03-15 13:44:50 -0400

    typeorm setup + custom config options file + removing unused gitignored

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+import getCustomOptions from './config';
+
+const options =  getCustomOptions();
+console.log(options.database)
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: options.envFilePath
+    }),
+    TypeOrmModule.forRoot(options.database),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
