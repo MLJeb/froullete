cb4b7ddd01fc38ef0ab4c0d8dca92652162de974
Jesus Beltran <jebeltranuseche@gmail.com>
2024-03-16 14:05:29 -0400

    running migrations on compose up

--- api/db/1710582302481-migrations.ts
+++ api/db/1710582302481-migrations.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Test1710581725968 implements MigrationInterface {
-    name = 'Test1710581725968'
+export class Migrations1710582302481 implements MigrationInterface {
+    name = 'Migrations1710582302481'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "props" ("slug" character varying NOT NULL, "readableName" character varying NOT NULL, "colour" character varying NOT NULL, CONSTRAINT "PK_908530107b29b533015e00827b6" PRIMARY KEY ("slug"))`);
